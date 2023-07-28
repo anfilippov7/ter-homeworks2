a4272b9576aaf391e56a20895c477ed5c220ae3f
Alexander Filippov <sash.f@rambler.ru>
2023-07-28 17:16:13 +0300

    task 2 one

--- 02/src/main.tf
+++ 02/src/main.tf
@@ -10,15 +10,15 @@ resource "yandex_vpc_subnet" "develop" {
 
 
 data "yandex_compute_image" "ubuntu" {
-  family = "ubuntu-2004-lts"
+  family = var.vm_web_family
 }
 resource "yandex_compute_instance" "platform" {
-  name        = "netology-develop-platform-web"
-  platform_id = "standart-v4"
+  name        = var.vm_web_name
+  platform_id = var.vm_web_platform_id
   resources {
-    cores         = 1
-    memory        = 1
-    core_fraction = 5
+    cores         = var.vm_web_cores
+    memory        = var.vm_web_memory
+    core_fraction = var.vm_web_core_fraction
   }
   boot_disk {
     initialize_params {
@@ -26,15 +26,47 @@ resource "yandex_compute_instance" "platform" {
     }
   }
   scheduling_policy {
-    preemptible = true
+    preemptible = var.vm_web_preemptible
   }
   network_interface {
     subnet_id = yandex_vpc_subnet.develop.id
-    nat       = true
+    nat       = var.vm_web_nat
   }
 
   metadata = {
-    serial-port-enable = 1
+    serial-port-enable = var.vm_web_serial_port_enable
+    ssh-keys           = "ubuntu:${var.vms_ssh_root_key}"
+  }
+
+}
+
+
+data "yandex_compute_image" "ubuntu_db" {
+  family = var.vm_db_family
+}
+resource "yandex_compute_instance" "platform_db" {
+  name        = var.vm_db_name
+  platform_id = var.vm_db_platform_id
+  resources {
+    cores         = var.vm_db_cores
+    memory        = var.vm_db_memory
+    core_fraction = var.vm_db_core_fraction
+  }
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu.image_id
+    }
+  }
+  scheduling_policy {
+    preemptible = var.vm_db_preemptible
+  }
+  network_interface {
+    subnet_id = yandex_vpc_subnet.develop.id
+    nat       = var.vm_db_nat
+  }
+
+  metadata = {
+    serial-port-enable = var.vm_db_serial_port_enable
     ssh-keys           = "ubuntu:${var.vms_ssh_root_key}"
   }
 
---
+++ 02/src/vms_platform.tf
@@ -0,0 +1,152 @@
+###cloud vars
+variable "token" {
+  type        = string
+  description = "OAuth-token; https://cloud.yandex.ru/docs/iam/concepts/authorization/oauth-token"
+}
+
+variable "cloud_id" {
+  type        = string
+  description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
+}
+
+variable "folder_id" {
+  type        = string
+  description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
+}
+
+variable "default_zone" {
+  type        = string
+  default     = "ru-central1-a"
+  description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
+}
+variable "default_cidr" {
+  type        = list(string)
+  default     = ["10.0.1.0/24"]
+  description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
+}
+
+variable "vpc_name" {
+  type        = string
+  default     = "develop"
+  description = "VPC network & subnet name"
+}
+
+variable "vm_web_family" {
+  type        = string
+  default     = "ubuntu-2004-lts"
+  description = "ubuntu release name"
+}
+
+variable "vm_web_name" {
+  type        = string
+  default     = "netology-develop-platform-web"
+  description = "name platform web"
+}
+
+variable "vm_web_platform_id" {
+  type        = string
+  default     = "standard-v3"
+  description = "standard platform VM"
+}
+
+variable "vm_web_cores" {
+  type        = number
+  default = 2
+  description = "cores value VM"
+}
+
+variable "vm_web_memory" {
+  type        = number
+  default = 1
+  description = "memory value VM"
+}
+
+variable "vm_web_core_fraction" {
+  type        = number
+  default = 20
+  description = "core_fraction value VM"
+}
+
+variable "vm_web_preemptible" {
+  type        = bool
+  default = true
+  description = "preemptible value VM"
+}
+
+variable "vm_web_nat" {
+  type        = bool
+  default = true
+  description = "nat value VM"
+}
+
+variable "vm_web_serial_port_enable" {
+  type        = number
+  default = 1
+  description = "serial_port_enable value VM"
+}
+
+
+
+variable "vm_db_family" {
+  type        = string
+  default     = "ubuntu-2004-lts"
+  description = "ubuntu release name"
+}
+
+variable "vm_db_name" {
+  type        = string
+  default     = "netology-develop-platform-db"
+  description = "name platform db"
+}
+
+variable "vm_db_platform_id" {
+  type        = string
+  default     = "standard-v3"
+  description = "standard platform VM"
+}
+
+variable "vm_db_cores" {
+  type        = number
+  default = 2
+  description = "cores value VM"
+}
+
+variable "vm_db_memory" {
+  type        = number
+  default = 2
+  description = "memory value VM"
+}
+
+variable "vm_db_core_fraction" {
+  type        = number
+  default = 20
+  description = "core_fraction value VM"
+}
+
+variable "vm_db_preemptible" {
+  type        = bool
+  default = true
+  description = "preemptible value VM"
+}
+
+variable "vm_db_nat" {
+  type        = bool
+  default = true
+  description = "nat value VM"
+}
+
+variable "vm_db_serial_port_enable" {
+  type        = number
+  default = 1
+  description = "serial_port_enable value VM"
+}
+
+
+
+###ssh vars
+
+variable "vms_ssh_root_key" {
+  type        = string
+  default     = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC8Ygnnb/G6ShIAV4X6ESqTgADZI+MfDaaPHvXoOjvCtxKmm1f2vMJV7HwnBHKCVmZT2JGwxixynZr/u6G/Z/RySMZZlcNidYmz76QMEghuhoCyhGxAavWDfnw1B1e43gSlAqMoVZe9+jZ/lqhC5GI1RGLGrssctppHF/1hpfgmgRjRs/Vc+6wD3++CfwfTXJHO6NX9TJ3+J4WamdD76pyvX+8DljD8e9L7js7wh9V6M3YwoV6u8G+TawaTivWa2Pv6/YtO5sXQxtu2HmhLtEb+WKN0D3i4JYSzwQ7rh1v+k00xzHgo5nW2/NaQSZ0xLUv3BFzQMoD/zi7oYdKY78JxXGkV8SA61zpVYUIbdNt6b0Q0l/TkZy1IBIghFMCFC51qqUwbvGEjFJxpnp2w/hXWMnb23Xtz3xp+W8UyxsKnRjtg11VrvGAqMYcCnf+z1xq+855xAvpP93Tjsvooks+nsTMsnxgMzDZVKqsBvZFTk+yMLdHbKSEOaOhwxXP4PD1IOOnXjQId1htGB0qexHwgYwyHxmwEqT0/O0Uu6mxzG0pdAsXyplOQZO5cWZRqQLFWzvaMWZ3ywaIqSPtvrgQLGy/hCnW3u8EO2RmRhECDLvx/VpUDC5JB2xjCd2UYmDC/WI4ioNCGXCMVOAyjnWY/snedhuGw5++DWOPfVmQDew=="
+  description = "ssh-keygen -t ed25519"
+}
