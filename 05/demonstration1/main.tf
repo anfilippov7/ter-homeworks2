48c77327d3eb8842887a40a6263680467e13d4ad
anfilippov7 <sash.f@rambler.ru>
2023-11-13 17:00:01 +0300

    terraform ci/cd4

--- 05/demonstration1/main.tf
+++ 05/demonstration1/main.tf
@@ -5,6 +5,17 @@ terraform {
     }
   }
   required_version = ">=0.13"
+
+backend "s3" {
+endpoint = "storage.yandexcloud.net"
+bucket = "tfstate-hw5"
+region = "ru-central1"
+key = "terraform.tfstate"
+skip_region_validation = true
+skip_credentials_validation = true
+
+}
+
 }
 
 provider "yandex" {
